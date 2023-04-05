b6d6451ea38ecf7c6282e92bc16d5210d7a64818
Wilfred Abruquah <wilfred.abruquah@turntabl.io>
2023-04-05 15:26:58 +0000

    Working Virtual Machine Builder

---
+++ src/test/java/org/example/VirtualMachineOrganisation/Builder/PCBuilderTest.java
@@ -0,0 +1,24 @@
+package org.example.VirtualMachineOrganisation.Builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PCBuilderTest {
+
+    @Test
+    void isAuthorised() {
+    }
+
+    @Test
+    void iisAuthorised() {
+    }
+
+    @Test
+    void createNewMachine() {
+    }
+
+    @Test
+    void buildMachine() {
+    }
+}
